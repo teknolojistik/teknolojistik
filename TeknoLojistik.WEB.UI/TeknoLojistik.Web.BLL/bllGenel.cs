4432391add24311d6dd9f30773cee2c414407538
Ossy <teknossy@gmail.com>
2020-04-23 01:18:42 +0300

    Epey çalıştık

---
+++ TeknoLojistik.WEB.UI/TeknoLojistik.Web.BLL/bllGenel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TeknoLojistik.Web.DAL;
+using TeknoLojistik.Web.DAL.DB;
+
+namespace TeknoLojistik.Web.BLL
+{
+    public class bllGenel
+    {
+        public static List<Sehir> ilListele()
+        {
+            return dalGenel.ilListele();
+        }
+
+        public static List<Ilce> ilceListele(int sehirId)
+        {
+            return dalGenel.ilceListele(sehirId);
+        }
+    }
+}
---
+++ TeknoLojistik.WEB.UI/TeknoLojistik.Web.DAL/dalGenel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TeknoLojistik.Web.DAL.DB;
+
+namespace TeknoLojistik.Web.DAL
+{
+    public class dalGenel
+    {
+        public static List<Sehir> ilListele()
+        {
+            TeknoLojistikContext ctx = new TeknoLojistikContext();
+
+            List<Sehir> lst = ctx.Sehirler.ToList();
+
+            return lst;
+        }
+
+        public static List<Ilce> ilceListele(int sehirId)
+        {
+            TeknoLojistikContext ctx = new TeknoLojistikContext();
+
+            List<Ilce> lst = ctx.Ilceler.Where<Ilce>(o => o.SehirId == sehirId).ToList();
+
+            return lst;
+        }
+    }
+}
